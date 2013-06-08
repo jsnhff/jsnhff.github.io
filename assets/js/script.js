3aa6999661deca9a2afb60d2a732de08fe9ca078
jsnhff <jsnhff@gmail.com>
2013-06-08 19:03:04 -0400

    Adding more images and updating styles.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,44 @@
 $( document ).ready(function() {
+	var baseTime = 2500;
+	var timeArray = [];
+	for (var i = 0; i < 4; i++) {
+		var minusTime = Math.floor(Math.random()*1000);
+		timeArray[i] = (baseTime+ minusTime);
+	};
+
 	$("#slideshow1").responsiveSlides({
 	    auto: true,
 	    random: true,
-	    pause: true
+	    pause: true,
+	    timeout: timeArray[0]
 	});
 	$("#slideshow2").responsiveSlides({
 	    auto: true,
-	    random: true
+	    random: true,
+	    pause: true,
+	    timeout: timeArray[1]
 	});
 	$("#slideshow3").responsiveSlides({
 	    auto: true,
-	    random: true
+	    random: true,
+	    pause: true,
+	    timeout: timeArray[2]
 	});
 	$("#slideshow4").responsiveSlides({
 	    auto: true,
-	    random: true
+	    random: true,
+	    pause: true,
+	    timeout: timeArray[3]
 	});
 
 	var myDate = new Date().getHours();
-	if(myDate > 0 && myDate <= 11) {
+	if(myDate > 5 && myDate <= 11) {
 		console.log('Good Morning!');
 		$('body').addClass('day');
 	} else if(myDate >= 12 && myDate < 18) {
 		console.log('Good afternoon!');
 		$('body').addClass('day');
-	} else if(myDate >= 18 && myDate <= 24) {
+	} else if(myDate >= 20 && myDate <= 5) {
 		console.log('Hello night owl!');
 		$('body').addClass('night');
 	}
